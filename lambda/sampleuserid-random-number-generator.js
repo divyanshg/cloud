4bec2a258579b3518a20fead5718f662f1e13ef4
divyanshg <divyanshg809@gmail.com>
2020-05-11 01:28:56 +0530

    test

--- lambda/sampleuserid-random-number-generator.js
+++ lambda/sampleuserid-random-number-generator.js
@@ -5,7 +5,7 @@ exports.handler = function (event, callback) {
     var MIN = parseInt(event.min);
     var MAX = parseInt(event.max);
 
-    const data = `<h1 style="color:blue">This is your Random Number : <i>${Math.floor(Math.random() * MAX) + MIN}</i></h1>`
+    const data = `<h1 style="color:green">This is your Random Number : <i>${Math.floor(Math.random() * MAX) + MIN}</i></h1>`
 
     if(callback == null || callback == '') return data
     callback(null, data);
