ab7a9d80f51e0ef5a03f48176e2b005b014fd257
divyanshg <divyanshg809@gmail.com>
2020-05-10 23:20:59 +0530

    sry

--- lambda/runtime.js
+++ lambda/runtime.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
         axios[api[0].method]("http://localhost:43401/number?min=10&max=20").then((response) => {
 
-            res.send(CircularJSON.stringify(response))
+            res.send(response.data)
 
         }).catch((error) => {
 
