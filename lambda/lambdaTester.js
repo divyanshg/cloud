07f30e0de48ee07fcf3d7d25e8956bc8caf3385f
divyanshg <divyanshg809@gmail.com>
2020-05-11 00:04:28 +0530

    test

--- lambda/lambdaTester.js
+++ lambda/lambdaTester.js
@@ -5,10 +5,15 @@ const prompt = require('prompt-sync')();
 var uModule = prompt("function name : ");
 
 var testFunction = async () => {
+    try{
     const mod = require(`./${uModule}`)
     await mod;
 
     console.log(mod.handler)
+    }
+    catch(e){
+        console.log("Cannot find function")
+    }
 }
 
 testFunction()
