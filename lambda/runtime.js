639f5f2e51947bbc89be7080de582ab68066be26
divyanshg <divyanshg809@gmail.com>
2020-05-10 23:14:25 +0530

    test

--- lambda/runtime.js
+++ lambda/runtime.js
@@ -32,7 +32,10 @@ app.get('/', (req, res) => {
         await userModule;
         axios[api[0].method]("http://localhost:43401/number?min=10&max=20").then((response) => {
             res.send(response)
-        })
+        }).catch((error) => {
+            // handle error
+            console.log(error);
+          })
     })
 
 })
